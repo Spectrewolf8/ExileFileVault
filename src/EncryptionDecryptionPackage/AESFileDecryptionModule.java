6f8bba3e882095b2095a0f33e1a391c907c48c29
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2023-02-07 19:53:53 +0500

    First Commit just added compression and encryption functionality

---
+++ src/EncryptionDecryptionPackage/AESFileDecryptionModule.java
@@ -0,0 +1,48 @@
+package EncryptionDecryptionPackage;
+
+import FilesPackage.FileToDeCrypt;
+import HashPackage.SHA512_HashGenerator;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class AESFileDecryptionModule {
+
+    public void decryptWithEcb(FileToDeCrypt fileToDeCrypt, String password) throws IOException,
+            NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        System.out.println("Starting Decryption of "+fileToDeCrypt+" with aes");
+
+        byte[] key = (SHA512_HashGenerator.generateHash(password).substring(0, 16)).getBytes(StandardCharsets.UTF_8);// 32 byte = 256 bit key length
+        byte[] initialVector = (SHA512_HashGenerator.generateHash(password).substring(16, 32)).getBytes(StandardCharsets.UTF_8);
+        try (FileInputStream in = new FileInputStream(fileToDeCrypt.getAbsoluteFilePath().toString());
+             FileOutputStream out = new FileOutputStream(fileToDeCrypt.getDestinationPath())) {
+            byte[] inputBuffer = new byte[1024];
+            int len;
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(initialVector));
+            while ((len = in.read(inputBuffer)) != -1) {
+                byte[] outputBuffer = cipher.update(inputBuffer, 0, len);
+                if (outputBuffer != null)
+                    out.write(outputBuffer);
+            }
+            byte[] outputBuffer = cipher.doFinal();
+            if (outputBuffer != null)
+                out.write(outputBuffer);
+        }
+        System.out.println("Done with encryption of "+fileToDeCrypt+" with aes");
+    }
+}
---
+++ src/HashPackage/SHA512_HashGenerator.java
@@ -0,0 +1,24 @@
+package HashPackage;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA512_HashGenerator {
+    public static String generateHash(String input) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+            byte[] byteMessageDigest = messageDigest.digest(input.getBytes());
+            BigInteger bigInteger = new BigInteger(1, byteMessageDigest);
+
+            StringBuilder hashText = new StringBuilder(bigInteger.toString(16));
+            while (hashText.length() < 32) {
+                hashText.insert(0, "0");
+            }
+            return hashText.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
